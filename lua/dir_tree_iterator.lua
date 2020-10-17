3fa118e46f7d5d8d8b0812e42f473be77e1d5135
Mathijs Bakker <mathijs_bakker@me.com>
2020-10-17 19:37:30 +0200

    Filename match '.cs' and 'Tests'

--- lua/dir_tree_iterator.lua
+++ lua/dir_tree_iterator.lua
@@ -3,9 +3,10 @@
 -- Get method names of [Tests] 
 -- Store them in a table { path, namespace, filename, method name }
 
-local M = {}
 local uv = require("luv")
 
+local M = {}
+local vim = vim
 local version = uv.version()
 
 local path = '~/Repositories/Sonitm/Net/SocialLibrary/Tests/FriendsTests'
@@ -13,13 +14,39 @@ local path = '~/Repositories/Sonitm/Net/SocialLibrary/Tests/FriendsTests'
 path = path:gsub("^~(/?)", uv.os_homedir().."%1")
 
 -- Check if is a valid dir
--- returns bool
+-- returns is_valid
 local function is_dir(type)
 	if type == "directory" then
 		print("IsDir == true")
 	else
 		print("IsDir == false")
 	end	
+end
+
+local function filename_match(name, str)
+	if string.match(name, str) then
+		print("It's a " .. str .. " file")
+		return true
+	else
+		print("It's not a " .. str .. " file")
+		return false
+	end
+end
+-- Check if is a valid file
+-- True: has .cs extension or optional contains 'Test' in filename
+-- Else return False
+local function is_valid_file(name)
+	local is_valid
+
+	is_valid = filename_match(name, ".cs")
+
+	if vim.g.testexplorer_filename_match_test then
+		is_valid = filename_match(name, "Test")
+	end
+
+	print(is_valid)
+
+	return is_valid
 end
 
 local files = {}
@@ -32,6 +59,7 @@ local function get_files_in_directory()
 		local name, type = uv.fs_scandir_next(h)
 		print(name .. " " .. type)
 		is_dir(type)
+		is_valid_file(name)
 		if not name then
 			break
 		end
