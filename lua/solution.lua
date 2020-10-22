dc2100d60a10bdafcb6c23f552aa6b0ae584b267
Mathijs Bakker <mathijs_bakker@me.com>
2020-10-22 10:04:09 +0200

    Add a link to a local .Net project solution file for testing purposes

--- lua/solution.lua
+++ lua/solution.lua
@@ -3,16 +3,22 @@ local api = vim.api
 
 local M = {}
 
-function M.omnisharp_sln()
+local function omnisharp_sln()
    local sln = vim.b.OmniSharp_host.sln_or_dir
 
    if sln == nil then
-      print("No solution file.")
+      echom("Cannot load a solution file. No omnisharp server running to provide one.")
+      echom("lua/solution.lua: Providing a .sln from Sonitm project for testing purposes...")
+      sln = '~/Repositories/Sonitm/Net/SocialLibrary/Tests/FriendsTests'
    else
       sln = string.match(sln, ".+/")
    end
 
    return sln
+end
+
+function Get()
+   omnisharp_sln()
 end
 
 return M
