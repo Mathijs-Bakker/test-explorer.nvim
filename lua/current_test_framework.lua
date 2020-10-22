bfe783ff2a51a18318d1d34089be980f7ade1e61
Mathijs Bakker <mathijs_bakker@me.com>
2020-10-22 11:44:46 +0200

    Read possible tests project links form solution

--- lua/current_test_framework.lua
+++ lua/current_test_framework.lua
@@ -1,10 +1,26 @@
 local current_test_framework = {}
 
-local vim = vim
+local solution_path = require("solution").get()
+
+local framework_data = require("test_framework_data")
+
 
 function get()
-   
+   local f_sln = assert(io.open(solution_path, "r"))
 
+   while true do
+      local line = io.read("*line")
+      if line == nil then break end
+      if string.match(line, "Global") then break end
+      
+      if string.match(string.lower(line), "tests") then
+         local candidate_project_path = string.match(line, '".*"')
+         if string.match(candidate_project_path, '.csproj') then
+            print("Add tests directory: " ... s)
+         end
+      end
+   end 
+   f_sln:close()
 end
 
 return current_test_framework
