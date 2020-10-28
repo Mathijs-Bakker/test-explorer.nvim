0c7a05ca15cafffe0f5d44e4b6a5c7a1965ce568
Mathijs Bakker <mathijs_bakker@me.com>
2020-10-28 10:40:54 +0100

    Wip: Create an absolute path from sln path and 'Project block' path

--- lua/wip.lua
+++ lua/wip.lua
@@ -2,6 +2,10 @@
 -- 'Project("{FAE04EC0}") = "ExampleTests", "tests\\ExampleTests\\ExampleTests.csproj", "{07FC1353}"'
 
 -- TODO: Returned path needs to be absolute and platform-dependent 
+
+local path = require("path")
+local solution = require("solution")
+
 local sln_project_block_converter = {}
 
 local strip_quotes = function(str)
@@ -32,11 +36,39 @@ end
 
 sln_project_block_converter.get = function(line)
    local path = get_candidate_project_path(line)
-   local relative_path = remove_filename_from_path(path)
+   local path_from_sln = remove_filename_from_path(path)
 
    local filename = get_filename_from_path(path)
    local project_name, project_type = get_project_properties(filename)
-   print("rel_path: " .. relative_path .. "\nproject_name: " .. project_name .. "\nproject_type: " .. project_type)
+   print("rel_path: " .. path_from_sln .. "\nproject_name: " .. project_name .. "\nproject_type: " .. project_type)
+
+   local uv = require("luv")
+
+   local filepath = path_from_sln:gsub("^~(/?)", uv.os_homedir().."%1")
+   -- local abs_path = path:make_absolute(path_from_sln)
+   print("absolute " .. filepath)
+
+   path.solution = function()
+      local sln = solution:get()
+      print("sln" .. sln)
+   end
 end
 
+local sln = solution:get()
+local sln_path = remove_filename_from_path(sln)
+
+print("sln: " .. sln)
+print("sln_path: " .. sln_path)
+
+local os = package.config:sub(1, 1) == '\\' and 'win' or 'unix'
+local path_from_sln = "my\\path\\" 
+if os =='unix' then
+   print("nox")
+  path = path:gsub("\\", "/")
+end
+
+local proj_path = sln_path .. path
+print("proj_path: " .. proj_path)
+
 return sln_project_block_converter
+-- lua require'wip'.get(Project("{FAE04EC0}") = "ExampleTests", "tests\\ExampleTests\\ExampleTests.csproj", "{07FC1353}"')
