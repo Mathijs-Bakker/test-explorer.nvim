82a64b97d7f3df976b02f5b073c0bfe1c27170bd
Mathijs Bakker <mathijs_bakker@me.com>
2020-10-28 20:56:46 +0100

    Refactor main method

--- lua/wip.lua
+++ lua/wip.lua
@@ -1,8 +1,3 @@
--- Takes a line from solution file and returns test project properties: project's absolute path, name and type (scproj/fsproj)
--- 'Project("{FAE04EC0}") = "ExampleTests", "tests\\ExampleTests\\ExampleTests.csproj", "{07FC1353}"'
-
--- TODO: Returned path needs to be absolute and platform-dependent 
-
 local path = require("path")
 local solution = require("solution")
 
@@ -13,7 +8,7 @@ local strip_quotes = function(str)
    return str
 end
 
-local get_candidate_project_path = function(line)
+local get_path_from_project_block = function(line)
    local quotes_dirty_project_path = string.match(line, '"%g*sproj"')
    local path = strip_quotes(quotes_dirty_project_path)
    return path
@@ -34,41 +29,30 @@ local get_project_properties = function(filename)
    return name, extension
 end
 
-sln_project_block_converter.get = function(line)
-   local path = get_candidate_project_path(line)
-   local path_from_sln = remove_filename_from_path(path)
+local create_absolute_path = function(project_rel_from_sln_path)
 
-   local filename = get_filename_from_path(path)
-   local project_name, project_type = get_project_properties(filename)
-   print("rel_path: " .. path_from_sln .. "\nproject_name: " .. project_name .. "\nproject_type: " .. project_type)
+   local sln = solution:get()
+   local sln_path = remove_filename_from_path(sln)
 
-   local uv = require("luv")
+   local os = package.config:sub(1, 1) == '\\' and 'win' or 'unix'
 
-   local filepath = path_from_sln:gsub("^~(/?)", uv.os_homedir().."%1")
-   -- local abs_path = path:make_absolute(path_from_sln)
-   print("absolute " .. filepath)
-
-   path.solution = function()
-      local sln = solution:get()
-      print("sln" .. sln)
+   if os =='unix' then
+      project_rel_from_sln_path = project_rel_from_sln_path:gsub("\\", "/")
    end
+
+   return sln_path .. project_rel_from_sln_path 
 end
 
-local sln = solution:get()
-local sln_path = remove_filename_from_path(sln)
+sln_project_block_converter.get = function(line)
+   local project_block_path = get_path_from_project_block(line)
+   project_path = remove_filename_from_path(project_block_path)
+   project_path = create_absolute_path(project_path)
+   project_path = path.make_absolute(project_path)
 
-print("sln: " .. sln)
-print("sln_path: " .. sln_path)
+   local filename = get_filename_from_path(project_block_path)
+   local project_name, project_type = get_project_properties(filename)
 
-local os = package.config:sub(1, 1) == '\\' and 'win' or 'unix'
-local path_from_sln = "my\\path\\" 
-if os =='unix' then
-   print("nox")
-  path = path:gsub("\\", "/")
+   return project_name, project_type, project_path
 end
-
-local proj_path = sln_path .. path
-print("proj_path: " .. proj_path)
 
 return sln_project_block_converter
--- lua require'wip'.get(Project("{FAE04EC0}") = "ExampleTests", "tests\\ExampleTests\\ExampleTests.csproj", "{07FC1353}"')
