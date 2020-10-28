6244161f3dbc796fa93ab21f2de2e16df7abe7e2
Mathijs Bakker <mathijs_bakker@me.com>
2020-10-28 21:02:38 +0100

    Refactor method naming

--- lua/path.lua
+++ lua/path.lua
@@ -1,14 +1,11 @@
-local path = {}
-
 local uv = require("luv")
+
+local path = {}
 
 path.separator = package.config:sub(1, 1)
 
-path.expand = function(relative_path)
-
+path.make_absolute = function(relative_path)
    local filepath = relative_path:gsub("^~(/?)", uv.os_homedir().."%1")
-
    return filepath
 end
-
 return path
