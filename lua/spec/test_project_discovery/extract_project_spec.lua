2e0655ddabe71c0308fecc7e9238ee441cdfe8fd
Mathijs Bakker <mathijs_bakker@me.com>
2021-01-24 23:44:41 +0100

    Update test names to their behavior

--- lua/spec/test_project_discovery/extract_project_spec.lua
+++ lua/spec/test_project_discovery/extract_project_spec.lua
@@ -1,77 +1,74 @@
---require ('plenary.test_harnes').setup_busted()
-
 require 'busted.runner'()
 
 local sut = require 'test_project_discovery.test_project_reader'
 
-describe("get_project_path(project_block):", function()
-   it("should return the c# project path", function()
+describe("Fetch the properties of a test project from a 'project block' (as found in a .NET solution file):\n", function()
+   describe("get_project_path() :", function()
+      it("Should return the path of a Csharp project", function()
 
-      local cs_project_block = 'Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "TestProject", "TestProject\\TestProject.csproj", "{0A9D5C8D-ECEB-42D3-B3CC-C71323CEE27C}"'
+         local cs_project_block = 'Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "TestProject", "TestProject\\TestProject.csproj", "{0A9D5C8D-ECEB-42D3-B3CC-C71323CEE27C}"'
 
-      local expected = 'TestProject\\TestProject.csproj'
+         local expected = 'TestProject\\TestProject.csproj'
 
-      assert.is_equal(expected, sut.get_project_path(cs_project_block))
+         assert.is_equal(expected, sut.get_project_path(cs_project_block))
 
+      end)
+
+      it("should return the path of a Fsharp project", function()
+
+         local fs_project_block = 'Project("{6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705}") = "MathService.Tests", "MathService.Tests\\MathService.Tests.fsproj", "{30EE6D40-36ED-438D-B92B-8574F9C132E6}"'
+
+         local expected = 'MathService.Tests\\MathService.Tests.fsproj'
+
+         assert.is_equal(expected, sut.get_project_path(fs_project_block))
+
+      end)
    end)
 
-   it("should return the f# project path", function()
+   describe("get_path_without_filename(path) :", function ()
+      it("Should return the path without the filename", function()
 
-      local fs_project_block = 'Project("{6EC3EE1D-3C4E-46DD-8F32-0CC8E7565705}") = "MathService.Tests", "MathService.Tests\\MathService.Tests.fsproj", "{30EE6D40-36ED-438D-B92B-8574F9C132E6}"'
+         local path = 'path\\to\\testproject.csproj'
 
-      local expected = 'MathService.Tests\\MathService.Tests.fsproj'
-
-      assert.is_equal(expected, sut.get_project_path(fs_project_block))
-
-   end)
-end)
-
-describe("get_path_without_filename(path)", function ()
-   it("should return the path with the filename stripped", function()
-
-      local path = 'a\\b\\TestProject.csproj'
-
-      local expected = "a\\b\\"
-      assert.is_equal(expected, sut.get_path_without_filename(path))
-   end)
-end)
-
-describe("get_filename_from_path(path):", function()
-   it("should return the filename from a full path", function()
-      local path = 'a\\b\\c.ext'
-      local expected = 'c.ext'
-
-      assert.is_equal(expected, sut.get_filename_from_path(path))
-   end)
-end)
-
-describe("get_project_properties(filename):", function()
-
-   local filename = 'a.ext'
-
-   it("should return 2 arguments", function()
-      assert.returned_arguments(2, sut.get_project_properties(filename))
+         local expected = 'path\\to\\'
+         assert.is_equal(expected, sut.get_path_without_filename(path))
+      end)
    end)
 
-   describe("returned values,", function()
+      describe("get_filename_from_path(path) :", function()
+      it("Should return the filename with extension", function()
+         local path = 'a\\b\\testproject.csproj'
+         local expected = 'testproject.csproj'
+
+         assert.is_equal(expected, sut.get_filename_from_path(path))
+      end)
+   end)
+
+   describe("get_project_properties(filename) :", function()
+      local filename = 'a.ext'
+
+      it("Should return 2 arguments", function()
+         assert.returned_arguments(2, sut.get_project_properties(filename))
+      end)
 
       local name, extension = sut.get_project_properties(filename)
 
-      it("should return the file name", function()
+      it("Should return the file name", function()
          assert.is_equal(name, 'a')
       end)
 
-      it("should return the file extension", function()
+      it("Should return the file extension", function()
          assert.is_equal(extension, 'ext')
       end)
    end)
-end)
 
-local solution = require 'test_project_discovery.solution'
--- local solution = require '~/Repositories/test-explorer.nvim/lua/test_project_discovery/solution'
 
-describe("create_absolute_path(relative_path)", function()
-   it("should return an absolute path", function()
-      local sln_mock = mock(solution)
+   local solution = require 'test_project_discovery.solution'
+   -- local solution = require '~/Repositories/test-explorer.nvim/lua/test_project_discovery/solution'
+
+   describe("create_absolute_path(relative_path)", function()
+      it("should return an absolute path", function()
+         local sln_mock = mock(solution)
+      end)
    end)
 end)
